9a447fa4c36d898525f2da8036833fa9fd9cd14d
VanshSharma <svansh880@gmail.com>
2025-02-02 16:31:26 +0530

    BackendURL Added In Post New Job

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -10,7 +10,6 @@ const JobDetails = () => {
   const [job, setJob] = useState({});
   const navigateTo = useNavigate();
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
-  console.log(BACKEND_URL);
   const { isAuthorized, user } = useContext(Context);
 
   useEffect(() => {
